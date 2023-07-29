3f8430f4ef4b8cd34ad117448c8461407213b555
Shehanf2003 <135214981+Shehanf2003@users.noreply.github.com>
2023-07-29 19:54:43 +0530

    Add files via upload

---
+++ Practical2/Practical 2q3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main
+{
+   int dt,tt,avg;
+    printf("Enter The Distance Travelled m");
+    scanf("%d",&dt);
+    printf("Enter The Time Taken s");
+    scanf("%d",&tt);
+    avg=dt/tt;
+    printf("Average Speed ms %d",avg);
+
+}
---
+++ Practical2/Practical 2q4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main
+{
+  float fh,cl;
+   printf("Enter The Fahrenheit Temperature degrees ");
+   scanf("%f",&fh);
+   cl=(fh-32)*5/9;
+   printf("Celsius Temperature degrees%f ",cl);
+
+}
